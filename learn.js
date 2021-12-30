62fbc3d1fc1f48e9ee8ffb727c19df5273efef8d
Defriansyah <95570832+Defriansyah@users.noreply.github.com>
2021-12-30 13:31:36 +0700

    Latihan 5.3: Melakukan operasi sederhana dengan operator perbandingan

--- learn.js
+++ learn.js
@@ -1,29 +1,43 @@
-// membuat variabel
-let myNumber = 16;
-let myString = "Hello"
-let myBoolean = true
-const phi = 3.14
-// menampilkan informasi ke console
-console.log(myNumber)
-console.log(myString)
-console.log(myBoolean)
-console.log(phi)
-// menampilkan informasi dalam bentuk alert pada window
-window.alert("Angka favorit saya adalah : " + myNumber)
-window.alert('Angka favorit saya adalah : ' + myNumber)
-window.alert(`Angka favorit saya adalah : ${myNumber}`)
+// // membuat variabel
+// let myNumber = 16;
+// let myString = "Hello"
+// let myBoolean = true
+// const phi = 3.14
+// // menampilkan informasi ke console
+// console.log(myNumber)
+// console.log(myString)
+// console.log(myBoolean)
+// console.log(phi)
+// // menampilkan informasi dalam bentuk alert pada window
+// window.alert("Angka favorit saya adalah : " + myNumber)
+// window.alert('Angka favorit saya adalah : ' + myNumber)
+// window.alert(`Angka favorit saya adalah : ${myNumber}`)
 
-let x = 7
-let y = 2
-console.log(x, y)
-console.log('x + y =', x + y)
-console.log('x - y =', x - y)
-console.log('x * y =', x * y)
-console.log('x / y =', x / y)
-console.log('x % y =', x % y)
-console.log('x++ =', x++)
-console.log('++x =', ++x)
-console.log('x-- =', x--)
-console.log('--x =', --x)
+// let x = 7
+// let y = 2
+// console.log(x, y)
+// console.log('x + y =', x + y)
+// console.log('x - y =', x - y)
+// console.log('x * y =', x * y)
+// console.log('x / y =', x / y)
+// console.log('x % y =', x % y)
+// console.log('x++ =', x++)
+// console.log('++x =', ++x)
+// console.log('x-- =', x--)
+// console.log('--x =', --x)
 
 //Latihan 5.2: Melakukan operasi sederhana dengan operator aritmatika
+
+let a = 7
+let b = 2
+console.log(a, b)
+console.log('a > b =', a > b)
+console.log('a < b =', a < b)
+console.log('a >= b =', a >= b)
+console.log('a <= b =', a <= b)
+console.log('a == b =', a == b)
+console.log('a != b =', a != b)
+console.log('a === b =', a === b)
+console.log('a !== b =', a !== b)
+
+//Latihan 5.3: Melakukan operasi sederhana dengan operator perbandingan
