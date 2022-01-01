2ce51294ded060623715ba09abf4b3ef41a5b754
Defriansyah <95570832+Defriansyah@users.noreply.github.com>
2022-01-01 12:24:41 +0700

    Latihan 5.6: Bermain dengan array

--- learn.js
+++ learn.js
@@ -56,21 +56,75 @@
 // //Latihan 5.4: Melakukan operasi sederhana dengan operator logika
 
 //fungsi luas persegi
-function luas_persegi(s){
-    return s * s
-}
+// function luas_persegi(s){
+//     return s * s
+// }
 
-//fungsi luar segitiga
-function luas_segitiga (a, t){
-    return 0.5 * a * t
-}
+// //fungsi luar segitiga
+// function luas_segitiga (a, t){
+//     return 0.5 * a * t
+// }
 
-//fungsi volume blok 
-function volume_balok(p,l,t){
-    return p * l * t
-}
+// //fungsi volume blok 
+// function volume_balok(p,l,t){
+//     return p * l * t
+// }
 
-//menampilkan hasil ke consol
-console.log("luas persegi = " + luas_persegi(7))
-console.log("luas segitiga = " + luas_segitiga(4,7))
-console.log("volume baok = " + volume_balok(2,9,4))
+// //menampilkan hasil ke consol
+// console.log("luas persegi = " + luas_persegi(7))
+// console.log("luas segitiga = " + luas_segitiga(4,7))
+// console.log("volume baok = " + volume_balok(2,9,4))
+
+// //Latihan 5.5. Membuat Fungsi
+
+
+// array dengan nama-nama hewan
+var animals = ["kucing", "harimau", "gajah", "burung", "ikan paus"];
+
+// menampilkan array
+console.log("data array belum diubah")
+console.log(animals)
+
+// ganti elemen array di index 1
+animals[1] = "merpati";
+
+// menampilkan array ke console
+console.log("data array setelah diubah")
+console.log(animals)
+
+// menambahkan item baru ke array
+animals.push("tikus")
+
+// menampilkan array ke console
+console.log("item baru ditambahkan")
+console.log(animals)
+
+console.log("menghilangkan data")
+console.log(animals.pop())
+console.log(animals)
+console.log("jumlah data = "+ animals.length)
+
+// menambahkan data array menggunakan unshift()
+animals.unshift("kerbau")
+
+// menampilkan data array
+console.log("menampilkan data array menggunakan unshift()")
+console.log(animals)
+
+// menghilangkan data pertama array menggunakan shift()
+console.log("menghilangkan data pertama menggunakan shift")
+console.log(animals.shift())
+
+// mengurutkan data array menggunakan sort()
+console.log("mengurutkan data array menggunakan sort")
+console.log(animals.sort())
+
+// mengurutkan data array tapi terbalik menggunakan reverse()
+console.log("mengurutkan data array menggunakan reverse")
+console.log(animals.reverse())
+
+// menampilkan data array
+console.log("data array")
+console.log(animals)
+
+//Latihan 5.6: Bermain dengan array
